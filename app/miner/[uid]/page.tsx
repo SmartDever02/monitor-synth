9cfe5db57bf317a3b955d61204845e1d37459b61
SmartDever02 <smart.jamesjin@gmail.com>
2025-02-26 18:29:47 -0800

    feat: current date +2

--- app/miner/[uid]/page.tsx
+++ app/miner/[uid]/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const PromptScoreChart = dynamic(() => import('./chart'), { ssr: false })
 
 export default async function MinerPage({ params }: { params: { uid: string } }) {
-  const currentDate = new Date(new Date().setDate(new Date().getDate() + 1)).toISOString().split('T')[0];
+  const currentDate = new Date(new Date().setDate(new Date().getDate() + 2)).toISOString().split('T')[0];
   const beforeDate = new Date(new Date().setDate(new Date().getDate() - 2)).toISOString().split('T')[0];
 
   const res = await fetch(`https://synth.mode.network/validation/scores/historical?from=${beforeDate}&to=${currentDate}&miner_uid=${params.uid}`)
